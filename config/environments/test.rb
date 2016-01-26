904cac4480bddc23967a931d16c4732b95f6318e
sjovanelly <steve.jovanelly@gmail.com>
2016-01-26 18:46:52 +0000

    Add account activations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
+  
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
